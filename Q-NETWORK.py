5cd0fe7e509fd8bc53ac730ac882116246e8ddf7
gaoruiwei <1013384678@qq.com>
2021-09-15 22:32:21 +0800

    第二次

--- Q-NETWORK.py
+++ Q-NETWORK.py
@@ -13,10 +13,10 @@ class Q(nn.Module):
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5)
         self.bn3 = nn.BatchNorm2d(32)
 
-        # 线性输入连接的数量取决于conv2d层的输出，因此取决于输入图像的大小，因此请对其进行计算。
+
         self.fc = nn.Linear(20*20*32, 10)
 
-    # 使用一个元素调用以确定下一个操作，或在优化期间调用batch。返回tensor([[left0exp,right0exp]...]).
+
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
